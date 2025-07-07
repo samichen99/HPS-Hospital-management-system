883601d84b8e0e446cffa676b4c0c3dac4491bcd
samichen99 <samichentouba77@gmail.com>
2025-07-07 17:14:57 +0100

    add pages/home and set ut routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './app.jsx'
+import React from 'react';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>🏥 Welcome to the Hospital Management System</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+}
+
+export default Home;
