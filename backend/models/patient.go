81748845e17f8ebcce916541e9c65124d5f2f0b8
samichen99 <samichentouba77@gmail.com>
2025-09-09 12:22:48 +0100

    api fixes

--- backend/models/patient.go
+++ backend/models/patient.go
@@ -1,14 +1,14 @@
 package models
 
-import "time"
+
 
 type Patient struct {
-	ID              int       `json:"id"`             
-	UserID          int       `json:"user_id"`         
-	FullName        string    `json:"full_name"`        
-	DateOfBirth     time.Time `json:"date_of_birth"`    
-	Gender          string    `json:"gender"`            
-	Phone           string    `json:"phone"`            
-	Address         string    `json:"address"`         
-	InsuranceNumber string    `json:"insurance_number"`  
+	ID              int       `json:"id"`
+	UserID          int       `json:"user_id"`
+	FullName        string    `json:"full_name"`
+	DateOfBirth     string    `json:"date_of_birth"`
+	Gender          string    `json:"gender"`
+	Phone           string    `json:"phone"`
+	Address         string    `json:"address"`
+	InsuranceNumber string    `json:"insurance_number"`
 }
